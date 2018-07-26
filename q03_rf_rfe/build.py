3d0e3bd758f84227b8ca053fe387f7525fa6b34e
Vagus30 <vishwas.tejaswi@gmail.com>
2018-07-26 11:03:49 +0000

    Done

--- q03_rf_rfe/build.py
+++ q03_rf_rfe/build.py
@@ -1,3 +1,4 @@
+# %load q03_rf_rfe/build.py
 # Default imports
 import pandas as pd
 
@@ -8,4 +9,19 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 # Your solution code here
+def rf_rfe(df):
+    model = RandomForestClassifier()
+    X,y = df.iloc[:,:-1], df.iloc[:,-1]
+    rfe = RFE(model,step=1)
+    rfe = rfe.fit(X,y)
+    ranking = list(rfe.ranking_)
+    rfe = RFE(model, round(len(X.columns) / 2, 0)).fit(X, y)
+    top_features = []
+
+    for i in range(len(rfe.ranking_)):
+        if rfe.ranking_[i] == 1:
+            top_features.append(X.columns[i])
+
+    return top_features
+rf_rfe(data)
 
--- q04_select_from_model/build.py
+++ q04_select_from_model/build.py
@@ -1,3 +1,4 @@
+# %load q04_select_from_model/build.py
 # Default imports
 from sklearn.feature_selection import SelectFromModel
 from sklearn.ensemble import RandomForestClassifier
@@ -8,3 +9,14 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Your solution code here
+def select_from_model(df):
+    X,y = df.iloc[:,:-1],df.iloc[:,-1]
+    np.random.seed(9)
+    model = RandomForestClassifier()
+    model_new = SelectFromModel(model)
+    sfm = model_new.fit(X,y)
+    feature_idx = sfm.get_support()
+    feature_name = X.columns[feature_idx]
+    return list(feature_name)
+select_from_model(data)
+
